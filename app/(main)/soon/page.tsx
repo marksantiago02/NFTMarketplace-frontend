8b70436266fdff437d17639d8e3fcf47ac160d2e
Steven Leal <mitsurukudo213@gmail.com>
2024-12-24 01:54:18 -0600

    Update page.tsx
    Co-Authored-By: MARK <133281370+BTC415@users.noreply.github.com>
    Co-Authored-By: Steven Piao <177725156+stevenpiao0822@users.noreply.github.com>
    Co-Authored-By: Robert <140535426+roberta484@users.noreply.github.com>
    Co-Authored-By: Ichikawa Hiroshi <157710040+0xichikawa@users.noreply.github.com>
    Co-Authored-By: Steven Leal <144204636+dodger213@users.noreply.github.com>

--- app/(main)/soon/page.tsx
+++ app/(main)/soon/page.tsx
@@ -2,7 +2,7 @@ export default function CommingSoon() {
     return (
       <div className="h-[100vh] flex flex-col justify-center items-center">
         <h2>Coming Soon. Be together with NYWC.</h2>
-        <p>You will be in for a new and fun user experience.</p>
+        <p>You will be in for a new and funny user experience.</p>
       </div>
     );
   }
